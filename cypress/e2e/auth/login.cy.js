596efe42f991624bd50fcce22326c5bc0c54ec88
unknown <65372269+laceywalkerr@users.noreply.github.com>
2026-02-21 12:59:41 -0600

    Minor code cleanup - login page

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -3,21 +3,21 @@ import LoginPage from "../../support/pageObjects/LoginPage";
 describe("Authentication Flow", () => {
   const loginPage = new LoginPage();
 
-beforeEach(() => {
-  loginPage.visit();
-});
+  beforeEach(() => {
+    loginPage.visit();
+  });
 
-it("logs in successfully with valid credentials", () => {
-  loginPage.login("standard_user", "secret_sauce");
+  it("logs in successfully with valid credentials", () => {
+    loginPage.login("standard_user", "secret_sauce");
 
-  cy.url().should("include", "/inventory.html");
-  cy.get(".inventory_list").should("be.visible");
-});
+    cy.url().should("include", "/inventory.html");
+    cy.get(".inventory_list").should("be.visible");
+  });
 
-it("shows error with invalid credentials", () => {
-  loginPage.login("invalid_user", "wrong_password");
+  it("shows error with invalid credentials", () => {
+    loginPage.login("invalid_user", "wrong_password");
 
-  loginPage.errorMessage().should("be.visible");
-  cy.url().should("not.include", "/inventory.html");
-});
+    loginPage.errorMessage().should("be.visible");
+    cy.url().should("not.include", "/inventory.html");
+  });
 });
